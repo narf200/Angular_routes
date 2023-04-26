3710fb2220827e298cd6216dc3885f3580087562
narf200 <narf200@gmail.com>
2023-04-26 16:45:17 +0200

    feat add Resolving Dynamic Data with the resolve Guard

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Data, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 
 @Component({
@@ -19,12 +19,17 @@ export class ServerComponent implements OnInit, OnDestroy {
     private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.paramSubscription = this.route.params
-      .subscribe((params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-      });
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    );
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.paramSubscription = this.route.params
+    //   .subscribe((params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   });
   }
 
   onEdit() {
